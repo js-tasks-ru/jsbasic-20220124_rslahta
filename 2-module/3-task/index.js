f4ba0f8d3536bbd738e124b776efae6d11044f73
siriri siri <rslahta@gmail.com>
2022-02-11 08:43:10 +0300

    12

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('Введите число 1: ','');
+    this.b = +prompt('Введите число 1: ','');
+  }
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
